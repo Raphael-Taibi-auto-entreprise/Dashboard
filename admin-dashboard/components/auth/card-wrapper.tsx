6d46f2d38456b12e154e59f761ea41410e2868b9
RaphaelTaibi <83208861+RaphaelTaibi@users.noreply.github.com>
2024-06-08 18:02:06 +0200

    Home page with redirect dor login and style for page

---
+++ admin-dashboard/components/auth/card-wrapper.tsx
@@ -0,0 +1,29 @@
+"use client";
+import {
+    Card,
+    CardContent,
+    CardFooter,
+    CardHeader,
+} from '@/components/ui/card';
+
+interface CardWrapperProps {
+    children: React.ReactNode;
+    headerLabel: string;
+    backButtonLabel: string;
+    backButtonHref: string;
+    showSocial?: boolean;
+};
+
+export const CardWrapper = ({
+    children,
+    headerLabel,
+    backButtonLabel,
+    backButtonHref,
+    showSocial = true,
+}: CardWrapperProps) => {
+    return (
+        <Card className="w-[400px] shadow-md">
+            {children}
+        </Card>
+    )
+};
