d6726657ba027f43e9a03c22271d51560499566b
Taibi Raphael <83208861+RaphaelTaibi@users.noreply.github.com>
2024-06-09 15:12:03 +0200

    Feature/login (#11)
    * Home page with redirect dor login and style for page
    
    * Button for Google & Githun Login
    
    * Header Login Card
    
    * Components link for redirect register page
    
    * add input shadxn for Login Form
    
    * Validation Form Field with Zod
    
    * ADD ShadcnUI Form with Error handling
    
    * Prettierformatting
    
    * Update Form error handling

---
+++ admin-dashboard/components/auth/login-form.tsx
@@ -0,0 +1,112 @@
+"use client";
+
+import { useForm } from "react-hook-form";
+import { useState, useTransition } from "react";
+import * as z from "zod";
+import { LoginSchema } from "@/schemas";
+import { zodResolver } from "@hookform/resolvers/zod";
+import {
+     Form,
+     FormControl,
+     FormField,
+     FormItem,
+     FormLabel,
+     FormMessage
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Button } from "@/components/ui/button";
+import { CardWrapper } from "@/components/auth/card-wrapper";
+import { FormError } from "@/components/form-error";
+import { FormSuccess } from "@/components/form-success";
+import { login } from "@/actions/login";
+
+export const LoginForm = () => {
+     const [error, setError] = useState<string | undefined>("");
+     const [success, setSuccess] = useState<string | undefined>("");
+     const [isPending, startTransition] = useTransition();
+     const form = useForm<z.infer<typeof LoginSchema>>({
+          resolver: zodResolver(LoginSchema),
+          defaultValues: {
+               email: "",
+               password: "",
+          },
+     });
+
+     const onSubmit = (values: z.infer<typeof LoginSchema>) => {
+          //console.log(values); Check the value ✅
+          setError("");
+          setSuccess("");
+          startTransition(() => {
+               login(values)
+                    .then((data) => {
+                         setError(data.error);
+                         setSuccess(data.success);
+                    });
+          });
+     };
+
+     return (
+          <CardWrapper
+               headerLabel="Welcome Back!"
+               backButtonLabel="Don't have an account?"
+               backButtonHref="/auth/register"
+               showSocial
+          >
+               <Form {...form}>
+                    <form
+                         onSubmit={form.handleSubmit(onSubmit)}
+                         className="space-y-6"
+                    >
+                         <div className="space-y-4">
+                              <FormField
+                                   control={form.control}
+                                   name='email'
+                                   render={({ field }) => (
+                                        <FormItem>
+                                             <FormLabel>Email</FormLabel>
+                                             <FormControl>
+                                                  <Input
+                                                       {...field}
+                                                       disabled={isPending}
+                                                       placeholder="jhon.doe@exemple.com"
+                                                       type="email"
+                                                  />
+                                             </FormControl>
+                                             <FormMessage />
+                                        </FormItem>
+                                   )}
+                              />
+                              <FormField
+                                   control={form.control}
+                                   name='password'
+                                   render={({ field }) => (
+                                        <FormItem>
+                                             <FormLabel>Password</FormLabel>
+                                             <FormControl>
+                                                  <Input
+                                                       {...field}
+                                                       disabled={isPending}
+                                                       placeholder="*****"
+                                                       type="password"
+                                                  />
+                                             </FormControl>
+                                             <FormMessage />
+                                        </FormItem>
+                                   )}
+                              />
+                         </div>
+                         <FormError message={error} />
+                         <FormSuccess message={success} />
+                         <Button
+                              disabled={isPending}
+                              type="submit"
+                              className="w-full rounded hover:bg-accent focus:ring-2 focus:ring-accent focus:ring-offset-2 focus:text-secondary-foreground"
+                         >
+                              Login
+                         </Button>
+                    </form>
+               </Form>
+          </CardWrapper>
+     );
+};
+
