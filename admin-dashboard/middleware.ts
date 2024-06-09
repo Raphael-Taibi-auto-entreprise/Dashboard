a172491d7d641c4e697278f500d072a8cff00396
RaphaelTaibi <83208861+RaphaelTaibi@users.noreply.github.com>
2024-06-09 17:26:02 +0200

    Protect routes

--- admin-dashboard/middleware.ts
+++ admin-dashboard/middleware.ts
@@ -1,15 +1,44 @@
-import authConfig from "@/lib/auth.config";
-import NextAuth from "next-auth";
+
+import NextAuth from 'next-auth';
+import authConfig from '@/lib/auth.config';
+import {
+    DEFAULT_LOGIN_REDIRECT,
+    publicRoutes,
+    authRoutes,
+    apiAuthPrefix,
+} from '@/routes';
+
 
 const { auth } = NextAuth(authConfig);
 
-export default auth((req) => {
+export default auth((req => {
+    const { nextUrl } = req;
     const isLoggedIn = !!req.auth;
-    console.log("ROUTE: ", req.nextUrl.pathname)
-    console.log("IS LOGGED IN: ", isLoggedIn)
-})
- 
+
+    const isApiAuthRoute = nextUrl.pathname.startsWith(apiAuthPrefix);
+    const isPublicRoute = publicRoutes.includes(nextUrl.pathname);
+    const isAuthRoute = authRoutes.includes(nextUrl.pathname);
+
+    if (isApiAuthRoute) {
+      return Promise.resolve();
+    }
+
+    if (isAuthRoute) {
+      if (isLoggedIn) {
+        return Response.redirect(new URL(DEFAULT_LOGIN_REDIRECT, nextUrl));
+      }
+      return Promise.resolve();
+    }
+
+    if (!isLoggedIn && !isPublicRoute) {
+      return Response.redirect(new URL('/auth/login', nextUrl));
+    }
+
+    return Promise.resolve();
+  }
+));
+
 // Optionally, don't invoke Middleware on some paths
 export const config = {
-    matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
-}
+  matcher: ['/((?!.*\\..*|_next).*)', '/', '/(api|trpc)(.*)'],
+};
